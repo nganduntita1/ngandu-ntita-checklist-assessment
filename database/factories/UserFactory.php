86f3c6a898c9482d452227dccb35be80814eb1b5
Ngandu Ntita <nganduntita1@github.com>
2026-05-09 11:41:08 +0200

    feat: add UserFactory with admin and auditor role states

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'role' => 'auditor',
         ];
     }
 
@@ -39,6 +40,26 @@ class UserFactory extends Factory
     {
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
+        ]);
+    }
+
+    /**
+     * Indicate that the user has the admin role.
+     */
+    public function admin(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'admin',
+        ]);
+    }
+
+    /**
+     * Indicate that the user has the auditor role.
+     */
+    public function auditor(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'role' => 'auditor',
         ]);
     }
 }
